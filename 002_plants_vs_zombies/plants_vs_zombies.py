87ad7980f8d9a6afc346ffafad39bcd4e7ba7e23
YuraS <yurastepanenko@gmail.com>
2023-07-10 23:08:52 +0300

    feat: add def plant_war + def validate_input(task 2 - ready)

--- 002_plants_vs_zombies/plants_vs_zombies.py
+++ 002_plants_vs_zombies/plants_vs_zombies.py
@@ -1,7 +1,8 @@
 """
 Завдання 2 – Рослини проти Зомбі
 
-Створіть функцію, яка прийматиме два масиву несортованих чисел (перший - масив рослин, що захищається, другий - атакуючий масив зомбі) і поверне boolean значення в залежність від того чи перемогли захисники.
+Створіть функцію, яка прийматиме два масиву несортованих чисел (перший - масив рослин, що захищається,
+другий - атакуючий масив зомбі) і поверне boolean значення в залежність від того чи перемогли захисники.
 
 ➡️ Умови:
 
@@ -9,7 +10,8 @@
 Якщо значення однакове, вони обидва гинуть.
 Якщо одне із значень відсутнє (різна довжина масивів), солдат з непустим значенням виживає.
 Щоб вижити, сторона, що обороняється, повинна мати більше тих, хто вижив, ніж атакуюча сторона.
-У випадку, якщо з обох боків однакова кількість людей, що вижили, перемагає команда з найбільшою початковою силою атаки. Якщо загальна сила атаки з обох сторін однакова, поверніть True.
+У випадку, якщо з обох боків однакова кількість людей, що вижили, перемагає команда з найбільшою початковою силою атаки.
+ Якщо загальна сила атаки з обох сторін однакова, поверніть True.
 Початкова сила атаки є сумою всіх значень у кожному масиві.
 Тести:
 
@@ -18,3 +20,46 @@ zombies=[ 1, 3, 5, 7 ] plants=[ 2, 4 ] -> False
 zombies=[ 1, 3, 5, 7 ] plants=[ 2, 4, 0, 8 ] -> True
 zombies=[ 2, 1, 1, 1 ] plants=[ 1, 2, 1, 1 ] -> True
 """
+
+zombies = [1, 3, 5, 7]
+plants = [2, 4, 0, 8 ]
+
+
+def validate_input(func):
+    def wrapper(plants, zombies):
+        if not all(isinstance(x, (int, float)) for x in plants + zombies):
+            raise ValueError("Both plants and zombies must contain only numbers")
+        return func(plants, zombies)
+    return wrapper
+
+
+@validate_input
+def plant_war(plants, zombies):
+    min_len = min(len(plants), len(zombies))
+    score_plants = 0
+    score_zombies = 0
+
+    for i in range(min_len):
+        if plants[i] > zombies[i]:
+            score_plants += 1
+        elif plants[i] < zombies[i]:
+            score_zombies += 1
+
+    if len(plants) > min_len:
+        score_plants += len(plants[min_len:])
+    elif len(zombies) > min_len:
+        score_zombies += len(zombies[min_len:])
+
+    if score_plants > score_zombies:
+        return True
+    elif score_zombies > score_plants:
+        return False
+    elif score_zombies == score_plants and len(plants) == len(zombies):
+        return True
+    elif score_zombies == score_plants and len(plants) > len(zombies):
+        return True
+    elif score_zombies == score_plants and len(plants) < len(zombies):
+        return False
+
+
+print(plant_war(plants, zombies))
